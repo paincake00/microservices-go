47b2051d1d70398d53aa0a061bdbef9d0740467d
paincake00 <podpovetniydenis@gmail.com>
2026-08-15 03:29:05 +0300

    feat: add testcontainers with Postgres for executing order repo tests local and remote (CI)

--- order/internal/repository/order/postgres/suite_test.go
+++ order/internal/repository/order/postgres/suite_test.go
@@ -1,11 +1,16 @@
 package memory
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"testing"
 	"time"
 
 	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 
 	"github.com/paincake00/microservices-go/order/internal/migrator"
 	"github.com/paincake00/microservices-go/order/internal/repository"
@@ -13,7 +18,10 @@ import (
 )
 
 const (
-	dbURI         = "postgres://postgres:postgres@localhost:5432/order-db?sslmode=disable"
+	dbUser = "postgres"
+	dbPass = "postgres"
+	dbName = "order-db"
+
 	migrationsDir = "../../../../migrations"
 
 	maxOpenCons    = 30
@@ -25,14 +33,57 @@ const (
 type RepoSuite struct {
 	suite.Suite
 
+	container testcontainers.Container
+	dbHost    string
+	dbPort    string
+
 	orderRepo repository.IOrderRepository
 }
 
 func (s *RepoSuite) SetupSuite() {
-	// TODO: добавить тестовый Postgres instance через testcontainers
+	ctx := context.Background()
 
+	// Запрос на выкачивание образа Postgres
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:17.0-alpine3.20",
+		ExposedPorts: []string{"5432/tcp"},
+		Env: map[string]string{
+			"POSTGRES_PASSWORD": dbPass,
+			"POSTGRES_USER":     dbUser,
+			"POSTGRES_DB":       dbName,
+		},
+		WaitingFor: wait.ForListeningPort("5432/tcp"),
+	}
+
+	// Создание контейнера
+	container, err := testcontainers.GenericContainer(
+		ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if err != nil {
+		log.Fatalf("could not start container: %v", err)
+	}
+	s.container = container
+
+	// Получение хоста контейнера (хост для БД)
+	host, err := container.Host(ctx)
+	if err != nil {
+		log.Fatalf("could not get host: %v", err)
+	}
+	s.dbHost = host
+
+	// Получение прокинутого порта для Postgres
+	port, err := container.MappedPort(ctx, "5432")
+	if err != nil {
+		log.Fatalf("could not get port for container: %v", err)
+	}
+	s.dbPort = port.Port()
+
+	// Создание клиента с пулом соединений, настройками и sql-билдером
 	pgClient, err := pgclient.New(
-		dbURI,
+		fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPass, s.dbHost, s.dbPort, dbName),
 		pgclient.MaxOpenCons(maxOpenCons),
 		pgclient.MinIdleCons(minIdleCons),
 		pgclient.MaxConIdleTime(maxConIdleTime),
@@ -49,7 +100,10 @@ func (s *RepoSuite) SetupSuite() {
 	s.orderRepo = NewPostgresOrderStorage(pgClient)
 }
 
-func (s *RepoSuite) TearDownSuite() {}
+func (s *RepoSuite) TearDownSuite() {
+	err := s.container.Terminate(context.Background())
+	s.Require().NoError(err)
+}
 
 func TestOrderRepoIntegration(t *testing.T) {
 	suite.Run(t, new(RepoSuite))
